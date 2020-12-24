f2ee256e2cec5ab3fc6cf9fbdb23c69f8b0d1cdc
Christoph Haarburger <christoph.haarburger@aristra.com>
2020-12-24 16:36:08 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Christoph Haarburger on 24.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
